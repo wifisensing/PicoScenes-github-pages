35493c700e17d38b5c7b4122d8bacc279d8720ab
amumu <136903859@qq.com>
2025-04-25 20:14:34 +0800

    update the path name

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -1,9 +1,9 @@
 import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
-  { text: "User Manual", icon: "about", link: "/README.md" },
+  { text: "manual", icon: "about", link: "/README.md" },
   { text: "Application Notes", icon: "book", link: "/appnotes/appnotes.md" },
-  { text: "API Docs", icon: "article", link: "/html/index.html", target: "_blank"},
+  { text: "API Docs", icon: "article", link: "/api_docs/index.html", target: "_blank"},
   { text: "GitHub", icon: "github", link: "https://github.com/wifisensing" },
   {
     text: "Issue Tracker",
